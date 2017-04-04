6d8fce807e695c2714e65670362995ebb05a9fe7
DanielArturoRestrepo <danielarturo.restrepo.camacho.2daw@iesvegademijas.es>
2017-04-04 13:40:56 +0200

    sdfghj

--- calculos/calpintura.php
+++ calculos/calpintura.php
@@ -1,4 +1,19 @@
 <?php
 
+  $resultado = 0;
+
+  if (isset($_POST['superficie'])) {
+
+    $superficie = $_POST['superficie'];
+    $rendimiento = $_POST['rendimiento'];
+    $manos = $_POST['manos'];
+
+    if ( $superficie > 0 && $rendimiento > 0 && $manos > 0 ) {
+      $resultado = ( $superficie * $manos ) / $rendimiento;
+    }
+
+    echo "<h4>Necesitas ".round($resultado)." litros de pintura.</h4>";
+  }
+
 
 ?>
